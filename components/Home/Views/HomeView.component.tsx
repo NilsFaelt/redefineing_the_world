2fa728c99c4ec8b1e74b1cbbbc7872dbc4b542ac
NilsFaelt <92431259+NilsFaelt@users.noreply.github.com>
2023-05-08 20:41:48 +0200

    changed img paralax

--- components/Home/Views/HomeView.component.tsx
+++ components/Home/Views/HomeView.component.tsx
@@ -11,14 +11,12 @@ export const HomeView = () => {
         <Parallax
           blur={{ min: -15, max: 15 }}
           bgImage={
-            "https://images.unsplash.com/photo-1588594276800-2de0522b3b73?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+            "https://images.unsplash.com/photo-1535025075092-5a1cf795130b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1473&q=80"
           }
           bgImageAlt=''
-          strength={-500}
+          strength={-200}
           style={{ height: "10rem" }}
-        >
-          test
-        </Parallax>
+        ></Parallax>
       </ParallaxContainer>
     </Container>
   );
