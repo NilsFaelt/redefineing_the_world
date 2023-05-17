ced3f43179b51bad775fe0d6dfd4574d22ab0a10
NilsFaelt <92431259+NilsFaelt@users.noreply.github.com>
2023-05-17 21:02:28 +0200

    test

---
+++ pages/our-projects/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+import React from "react";
+import { OurProjectsView } from "../../components/OurProjects";
+
+import MainLayout from "../../layout/MainLayout";
+
+const index: NextPage = () => {
+  return <OurProjectsView />;
+};
+
+export default index;
