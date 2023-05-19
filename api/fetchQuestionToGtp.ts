5b9c179a440a85f7812aefb0f7053df7e65d6214
NilsFaelt <92431259+NilsFaelt@users.noreply.github.com>
2023-05-19 08:49:46 +0200

    test

--- api/fetchQuestionToGtp.ts
+++ api/fetchQuestionToGtp.ts
@@ -2,12 +2,12 @@ export const fetchQuestionToGtp = async (question: string, key: string) => {
   const questionRules = {
     role: "system",
     content:
-      "you will be asked questions by a customer on a software company, anser as if you were a developer at the company",
+      "you will be asked questions by a customer on a software company, answer as if you were a developer at the company",
   };
   const asistant = {
-    role: "asistant",
+    role: "assistant",
     content:
-      "Answer in the style of dalai lama, end all answers with, for more information mail: NILS.FAELT@GMAIL:COM",
+      "Answer in the style of dalai lama, end all answers with, for more information mail: NILS.FAELT@GMAIL:COM, if asked about price, tell to see priceing under the pricing tab in the menu",
   };
 
   const response = await fetch("https://api.openai.com/v1/chat/completions", {
