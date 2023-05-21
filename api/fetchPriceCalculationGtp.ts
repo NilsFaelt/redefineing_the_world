153026ba8993e62610f215d4a1bd6112666c225b
NilsFaelt <92431259+NilsFaelt@users.noreply.github.com>
2023-05-21 18:18:33 +0200

    test

--- api/fetchPriceCalculationGtp.ts
+++ api/fetchPriceCalculationGtp.ts
@@ -5,11 +5,12 @@ export const fetchPriceCalculationGtp = async (
   const questionRules = {
     role: "system",
     content:
-      "you are a working as customer service for a software company, and you will be asked for a price for a software-project, give the price you think is accurate with the information you get and use theese guidlines:project-type:portfoolio: 50 000 - 60 000sek, webstore 200 000 - 250 000sek, social-media 500 000 or more, app 150 000 - 300 000sek depending on complexity, design: none provided 15000 - 20000sek extra, ai: basic = 15 000 sek extra, more advanced ai 30 000 sek, payment: 20 000 - 40 000 depending on complexity, end with the accumilated price  ",
+      "you are a working as customer service for a software company, and you will be asked for a price for a software-project. ",
   };
   const asistant = {
     role: "assistant",
-    content: "answer in the style of Dalai Lama",
+    content:
+      "answer in the style of Dalai Lama and give  the price you think is accurate with the information you get and use theese guidlines:project-type:portfoolio: 50 000 - 60 000sek, webstore 200 000 - 250 000sek, social-media 500 000 or more, app 150 000 - 300 000sek depending on complexity, design: none provided 15000 - 20000sek extra, ai: basic = 15 000 sek extra, more advanced ai 30 000 sek, payment: 20 000 - 40 000 depending on complexity, end with the accumilated price ",
   };
 
   const response = await fetch("https://api.openai.com/v1/chat/completions", {
@@ -29,8 +30,8 @@ export const fetchPriceCalculationGtp = async (
         },
       ],
 
-      max_tokens: 250,
-      temperature: 0.4,
+      max_tokens: 350,
+      temperature: 0.1,
       // n: 1,
       // stop: ["\n"],
     }),
