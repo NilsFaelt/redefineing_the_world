304977a78338e10c3692739d991d4e75020b56f2
NilsFaelt <92431259+NilsFaelt@users.noreply.github.com>
2023-05-21 09:15:05 +0200

    up

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  serverRuntimeConfig: {
+    // Set the timeout value in milliseconds (e.g., 10 minutes)
+    apiTimeout: 20000,
+  },
 };
 
 module.exports = nextConfig;
