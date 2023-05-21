0c3e02babe680c90eafd77f6bb802715dbf54866
NilsFaelt <92431259+NilsFaelt@users.noreply.github.com>
2023-05-21 15:19:56 +0200

    up

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  serverRuntimeConfig: {
+    serverTimeout: 20000,
+  },
 };
 
 module.exports = nextConfig;
