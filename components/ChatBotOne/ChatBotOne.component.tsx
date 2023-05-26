e48f9f2b9d7c4d85fc70bfcbf300d21a00c95f35
NilsFaelt <92431259+NilsFaelt@users.noreply.github.com>
2023-05-26 20:39:17 +0200

    changed context

--- components/ChatBotOne/ChatBotOne.component.tsx
+++ components/ChatBotOne/ChatBotOne.component.tsx
@@ -1,6 +1,7 @@
 import { useRouter } from "next/router";
-import React, { FC, useRef, useEffect } from "react";
+import React, { FC, useRef, useEffect, useContext } from "react";
 import { useState } from "react";
+import { ToggleLandingContext } from "../../context /ToggleLandingContext";
 import { BaseInput, Button } from "../../ui/actions";
 import { findWord } from "../TestBot/components/utils";
 import { ChatBotAnswer } from "./ChatBotAnswer";
@@ -24,13 +25,11 @@ export const ChatBotOne: FC<{ companyGudlines: string }> = ({
   companyGudlines,
 }) => {
   const router = useRouter();
+  const { dialogInfo, setDialogInfo } = useContext(ToggleLandingContext);
   const [toggleChat, setToggleChat] = useState(false);
   const [loading, setLoading] = useState(false);
   const [question, setQuestion] = useState("");
   const [botRecentAnswer, setBotRecentAnswer] = useState("");
-  const [dialog, setDialog] = useState<DialogInfoType[]>([
-    { type: "bot", message: "Hello my friend, how can i help you today?" },
-  ]);
 
   const questionRules = {
     role: "system",
@@ -61,7 +60,7 @@ export const ChatBotOne: FC<{ companyGudlines: string }> = ({
 
   useEffect(() => {
     scrollToBottom();
-  }, [textAreaRef.current?.scrollHeight, dialog]);
+  }, [textAreaRef.current?.scrollHeight, dialogInfo]);
 
   const scrollToBottom = () => {
     textAreaRef.current?.scrollTo({
@@ -71,7 +70,7 @@ export const ChatBotOne: FC<{ companyGudlines: string }> = ({
   };
 
   const handleClick = async (message: string, type: "bot" | "user") => {
-    setDialog((prev) => [...prev, { message, type }]);
+    setDialogInfo((prev) => [...prev, { message, type }]);
     setQuestion("");
     if (question === "") return null;
 
@@ -91,7 +90,7 @@ export const ChatBotOne: FC<{ companyGudlines: string }> = ({
     const answer = data.choices?.[0].message.content
       ? data.choices?.[0].message.content
       : "Please ask again i didnt understand";
-    setDialog((prev) => [
+    setDialogInfo((prev) => [
       ...prev,
       { type: "bot", message: answer ? answer : "" },
     ]);
@@ -138,7 +137,7 @@ export const ChatBotOne: FC<{ companyGudlines: string }> = ({
           zindex={toggleChat ? 0 : -1}
           opacity={toggleChat ? 1 : 0}
         >
-          {dialog.map((each, i) => {
+          {dialogInfo.map((each, i) => {
             if (each.type === "user") {
               return <UserAnswer text={each.message} key={i} />;
             }
--- context /ToggleLandingContext.tsx
+++ context /ToggleLandingContext.tsx
@@ -6,23 +6,36 @@ import React, {
   SetStateAction,
   useState,
 } from "react";
+interface DialogInfoType {
+  type: "bot" | "user";
+  message: string;
+}
 
 export interface ToogleLandingContextValue {
   toogleLanding: boolean;
   setToogleLanding: Dispatch<SetStateAction<boolean>>;
+  dialogInfo: DialogInfoType[];
+  setDialogInfo: React.Dispatch<React.SetStateAction<DialogInfoType[]>>;
 }
 //test
 export const ToggleLandingContext = createContext<ToogleLandingContextValue>({
   toogleLanding: false,
   setToogleLanding: () => {},
+  dialogInfo: [],
+  setDialogInfo: () => {},
 });
 
 export const ToggleLandingContextProvider: FC<{
   children: ReactNode;
 }> = ({ children }) => {
+  const [dialogInfo, setDialogInfo] = useState<DialogInfoType[]>([
+    { type: "bot", message: "Hello my friend, how can i help you today?" },
+  ]);
   const [toogleLanding, setToogleLanding] = useState(true);
   return (
-    <ToggleLandingContext.Provider value={{ toogleLanding, setToogleLanding }}>
+    <ToggleLandingContext.Provider
+      value={{ dialogInfo, toogleLanding, setToogleLanding, setDialogInfo }}
+    >
       {children}
     </ToggleLandingContext.Provider>
   );
