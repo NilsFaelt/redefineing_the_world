e2b4ce2c3250c5b1ec57af0c1582f16e3cf12c9b
NilsFaelt <92431259+NilsFaelt@users.noreply.github.com>
2023-05-31 07:24:23 +0200

    added logo for google

--- layout/MainLayout.tsx
+++ layout/MainLayout.tsx
@@ -3,6 +3,8 @@ import { CompanyGuideLines } from "../constants/ChatBotInfo";
 import { Footer } from "../components/Footer/Footer.component";
 import { Header } from "../components/Header";
 import { ChatBotOne } from "../components/TestBot";
+import Head from "next/head";
+import Link from "next/link";
 
 type MainLayoutProps = {
   children: ReactNode;
@@ -11,6 +13,9 @@ type MainLayoutProps = {
 const MainLayout: FC<MainLayoutProps> = ({ children }) => {
   return (
     <div>
+      <Head>
+        <Link rel='icon' href='/logo-favicon.png' type='image/png' />
+      </Head>
       <Header />
       {children}
       <ChatBotOne companyGudlines={CompanyGuideLines} />
